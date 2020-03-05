1a9daeb9d448a73da5f143a8c7b6600d5c78e540
FantasticFiasco <mattias.kindborg@gmail.com>
2020-03-05 23:22:31 +0100

    wip

---
+++ test/Connection,spec.ts
@@ -0,0 +1,28 @@
+import * as chai from 'chai';
+
+import { Connection, Protocol } from './../src';
+
+chai.should();
+
+describe('connection', function() {
+
+    describe('#url', function() {
+
+        it('should return URL given HTTP protocol', function() {
+            // Act
+            const connection = new Connection(Protocol.Http, '1.2.3.4', 80, 'root', 'pass');
+
+            // Assert
+            connection.url.should.equal('http://1.2.3.4:80');
+        });
+
+        it('should return URL given HTTPS protocol', function() {
+            // Act
+            const connection = new Connection(Protocol.Https, '1.2.3.4', 80, 'root', 'pass');
+
+            // Assert
+            connection.url.should.equal('https://1.2.3.4:80');
+        });
+
+    });
+});
