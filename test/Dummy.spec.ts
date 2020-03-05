9fbbaa9a5fe6580a16fedcbafa5279226cf89ced
FantasticFiasco <mattias.kindborg@gmail.com>
2020-03-05 16:52:00 +0100

    test: add dummy test

---
+++ test/Dummy.spec.ts
@@ -0,0 +1,14 @@
+import * as chai from 'chai';
+
+chai.should();
+
+describe('Dummy', () => {
+
+    describe('#dummy', () => {
+
+        it('should do nothing', function () {
+        });
+
+    });
+
+});
