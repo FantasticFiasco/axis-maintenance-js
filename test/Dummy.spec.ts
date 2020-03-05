465d46f69e84add0d59bacb01103231ce75f0074
FantasticFiasco <mattias.kindborg@gmail.com>
2020-03-05 20:38:23 +0100

    chore: make sure coverage pass

--- test/Dummy.spec.ts
+++ test/Dummy.spec.ts
@@ -1,12 +1,16 @@
 import * as chai from 'chai';
 
+import { add } from '../src/Dummy';
+
 chai.should();
 
 describe('Dummy', () => {
 
-    describe('#dummy', () => {
+    describe('#add', () => {
 
-        it('should do nothing', function () {
+        it('should add two numbers', function () {
+            const result = add(1, 3);
+            result.should.equal(4);
         });
 
     });
