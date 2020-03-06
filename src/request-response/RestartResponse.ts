d61df1646643ba88b53ffa165149d0bfdaf74ea3
FantasticFiasco <mattias.kindborg@gmail.com>
2020-03-06 23:26:46 +0100

    wip

--- src/request-response/RestartResponse.ts
+++ src/request-response/RestartResponse.ts
@@ -1,12 +1,16 @@
+import { UnknownError } from '../errors';
 import { Response } from './Response';
 
 export class RestartResponse extends Response {
+    private static readonly SuccessResponse = /restartMessage\.shtml/i;
 
     constructor(response: string) {
         super(response);
     }
 
     public assertSuccess() {
-        // TODO: Implement
+        if (!RestartResponse.SuccessResponse.test(this.response)) {
+            throw new UnknownError('Request to restart device was not successful');
+        }
     }
 }
