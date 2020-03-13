b16231ddd28480aa260a3bec263f672df0e5f9c7
FantasticFiasco <mattias.kindborg@gmail.com>
2020-03-13 08:05:49 +0100

    wip

---
+++ test/factory-default/FactoryDefaultRequest.spec.ts
@@ -0,0 +1,31 @@
+import * as chai from 'chai';
+
+import { Connection, FactoryDefaultType, Protocol } from '../../src';
+import { FactoryDefaultRequest } from '../../src/factory-default/FactoryDefaultRequest';
+
+chai.should();
+
+describe('factory default request', function() {
+
+    const connection = new Connection(Protocol.Http, '1.2.3.4', 5678, 'root', 'pass');
+
+    describe('#url', function() {
+
+        it('should return partial type URL', function() {
+            // Act
+            const request = new FactoryDefaultRequest(connection, FactoryDefaultType.Partial);
+
+            // Assert
+            request.url.should.equal(`${connection.url}/axis-cgi/factorydefault.cgi`);
+        });
+
+        it('should return hard type URL', function() {
+            // Act
+            const request = new FactoryDefaultRequest(connection, FactoryDefaultType.Hard);
+
+            // Assert
+            request.url.should.equal(`${connection.url}/axis-cgi/hardfactorydefault.cgi`);
+        });
+
+    });
+});
